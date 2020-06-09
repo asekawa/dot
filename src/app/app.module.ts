65017e1b0209fb3af188d4ca3b33344522f3738a
asekawa <40081059+asekawa@users.noreply.github.com>
2020-06-09 20:42:20 +0530

    dotitude

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatModule } from "./mat/mat.module";
+import { InternsComponent } from './interns/interns.component';
+import { AudienceComponent } from './audience/audience.component';
+import { HistComponent } from './hist/hist.component';
+import { LinecComponent } from './linec/linec.component';
+import { IntcvComponent } from './intcv/intcv.component';
+import { IndexComponent } from './index/index.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InternsComponent,
+    AudienceComponent,
+    HistComponent,
+    LinecComponent,
+    IntcvComponent,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
